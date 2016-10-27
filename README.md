543bf3751c4b5e7d437a638e9a9656d21a1a2013
johnnybionic <jdavddlo@gmail.com>
2016-10-27 16:53:09 +0100

    Split main into methods

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-"# import-osm-mongo" 
+# import-osm-mongo 
 
 This is a simple app that's used to read an OpenStreetMap export file (OSM, in XML format) and insert into a 
 MongoDB collection. 
 
-It's part of a Coffee Shop application that originated as a presentation shown here:
+It's the first step in adapting a Coffee Shop application that originated as a presentation shown here:
 
 https://www.infoq.com/presentations/demo-java-javascript-mongodb
 
@@ -13,4 +13,12 @@ from OpenStreetMap - an example file is included.
 
  I've created a separate project as the Spring Boot-managed MongoDB drivers are out-of-date (v2 vs. v3.x), and do
  not support the Document or Point types. 
+ 
+ This project makes use of BasicOSMParser here:
+ 
+ https://github.com/PanierAvide/BasicOSMParser
+ 
+ For information on how to extract OSM data refer to my blog entry:
+ 
+ http://www.johnnybionic.me.uk/wordpress/2016/10/26/revisiting-the-coffee-shop-apps/
 
--- src/main/java/importer/Main.java
+++ src/main/java/importer/Main.java
@@ -27,7 +27,6 @@ import info.pavie.basicosmparser.model.Node;
  * 
  * Refer to: https://github.com/PanierAvide/BasicOSMParser
  * 
- * 
  * @author johnny
  *
  */
@@ -43,31 +42,18 @@ public class Main {
 
 		try {
 
+			// read the input file
 			Map<String, Element> result = p.parse(osmFile);
-			Map<String, Node> found = new HashMap<>();
 
 			if (result == null) {
-				System.out.println("null....");
-			} else {
-				result.keySet().forEach(key -> {
-					Element element = result.get(key);
-					// not necessary if you get the export right :)
-					if (element instanceof Node) {
-						Map<String, String> tags = element.getTags();
-						// again, no need for these checks if the export only
-						// gets the required tags
-						// - but getting that working isn't the easiest task...
-						if (tags.containsKey("amenity") && tags.get("amenity").equalsIgnoreCase("cafe")) {
-							found.put(key, (Node) element);
-						}
-						if (tags.containsKey("cuisine") && tags.get("cuisine").equalsIgnoreCase("coffee_shop")) {
-							found.put(key, (Node) element);
-						}
-					}
-				});
-
+				System.out.println("Parsing resulted in null");
+				return;
 			}
 
+			// extract and convert
+			Map<String, Node> found = convertInputFile(result);
+
+			// open the database and get the collection
 			MongoClientURI uri = new MongoClientURI(DB_HOST);
 			MongoClient client = new MongoClient(uri);
 			MongoDatabase db = client.getDatabase("coffeeshop");
@@ -75,37 +61,82 @@ public class Main {
 			MongoCollection<Document> collection = db.getCollection("coffeeshops");
 			collection.drop();
 
-			List<Document> documents = new LinkedList<>();
+			// convert to MongoDB documents
+			List<Document> documents = convertToDocuments(found); 
 
-			found.keySet().forEach(key -> {
-
-				Node x = found.get(key);
-				if (x.getTags().containsKey("name")) {
-					Position coordinate = new Position(x.getLon(), x.getLat());
-					Point point = new Point(coordinate);
-					Document document = new Document("openStreetMapId", x.getId()).append("location", point)
-							.append("timestamp", x.getTimestamp()).append("user", x.getUser()).append("node", key);
-
-					x.getTags().keySet().forEach(tag -> {
-						document.append(tag.replaceAll("\\.", "_"), x.getTags().get(tag).toString());
-					});
-
-					documents.add(document);
-					// collection.insertOne(document);
-					// System.out.println(x);
-
-				}
-			});
-
+			// add all and create index (required for geospatial search)
 			collection.insertMany(documents);
 			System.out.println(collection.count() + " records inserted");
 			collection.createIndex(new Document("location", "2dsphere"));
 
 			client.close();
 
-		} catch (IOException | SAXException e) {
+		} 
+		catch (IOException | SAXException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * Converts to MongoDB {@link Document}s to be inserted into the database.
+	 * 
+	 * @param found a map of entries to convert, must not be null, but can be empty.
+	 * 
+	 * @return a {@link List} of {@link Document}
+	 */
+	private static List<Document> convertToDocuments(Map<String, Node> found) {
+		List<Document> documents = new LinkedList<>();
+
+		found.keySet().forEach(key -> {
+
+			Node x = found.get(key);
+			if (x.getTags().containsKey("name")) {
+				Position coordinate = new Position(x.getLon(), x.getLat());
+				Point point = new Point(coordinate);
+				Document document = new Document("openStreetMapId", x.getId()).append("location", point)
+						.append("timestamp", x.getTimestamp()).append("user", x.getUser()).append("node", key);
+
+				x.getTags().keySet().forEach(tag -> {
+					document.append(tag.replaceAll("\\.", "_"), x.getTags().get(tag).toString());
+				});
+
+				documents.add(document);
+			}
+		});
+
+		return documents;
+	}
+
+	/**
+	 * Extracts all {@link Node}s from the results that are cafes or coffee shops.
+	 * Note that this step is redundant if the result set obtained from OpenStreetMap is
+	 * correct, and only contains the required data. 
+	 *  
+	 * @param result the imported OSM file.
+	 * @return a {@link Map} of filtered data
+	 */
+	private static Map<String, Node> convertInputFile(Map<String, Element> result) {
+		
+		Map<String, Node> found = new HashMap<>();
+		
+		result.keySet().forEach(key -> {
+			Element element = result.get(key);
+			// not necessary if you get the export right :)
+			if (element instanceof Node) {
+				Map<String, String> tags = element.getTags();
+				// again, no need for these checks if the export only
+				// gets the required tags
+				// - but getting that working isn't the easiest task...
+				if (tags.containsKey("amenity") && tags.get("amenity").equalsIgnoreCase("cafe")) {
+					found.put(key, (Node) element);
+				}
+				if (tags.containsKey("cuisine") && tags.get("cuisine").equalsIgnoreCase("coffee_shop")) {
+					found.put(key, (Node) element);
+				}
+			}
+		});
+
+		return found;
 	}
 
 }
